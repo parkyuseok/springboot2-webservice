efe7421b772fddb3a5b3e1c4d8f127dd39d412f1
parkyuseok <znql16@gmail.com>
2022-01-10 16:09:35 +0900

    feat: DTO 테스트 코드 작성하기

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -25,6 +25,21 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 }
@@ -63,4 +78,14 @@ public class HelloControllerTest {
  * - mvc.perform의 결과를 검증합니다.
  * - 응답 본문의 내용을 검증합니다.
  * - Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증합니다.
+ *
+ * 8. param
+ * - API 테스트할 때 사용될 요청 파라미터를 설정합니다.
+ * - 단, 값은 String만 허용됩니다.
+ * - 그래서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능합니다.
+ *
+ * 9. jsonPath
+ * - JSON 응답값을 필드별로 검증할 수 있는 메소드입니다.
+ * - $를 기준으로 필드명을 명시합니다.
+ * - 여기서 name과 amount를 검증하니 $.name, $.amount로 검증합니다.
  */
